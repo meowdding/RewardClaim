fb4cd43dd74f9977d277304cd66f4fd167be1b3f
Gravy Boat <gravy@thatgravyboat.tech>
2025-09-07 13:37:16 -0230

    fix: data not parsing for rewards that contain \'

--- src/main/kotlin/tech/thatgravyboat/rewardclaim/data/RewardState.kt
+++ src/main/kotlin/tech/thatgravyboat/rewardclaim/data/RewardState.kt
@@ -1,7 +1,9 @@
 package tech.thatgravyboat.rewardclaim.data
 
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonElement
+import com.google.gson.Strictness
 import com.mojang.serialization.JsonOps
-import net.minecraft.util.GsonHelper
 
 data class RewardState(
     val token: String,
@@ -11,14 +13,18 @@ data class RewardState(
 
     companion object {
 
+        private val GSON = GsonBuilder()
+            .setStrictness(Strictness.LENIENT)
+            .create()
+
         fun get(security: MatchResult, data: MatchResult, i18n: MatchResult): RewardState {
             val token = security.groups["token"]?.value ?: error("Security token not found")
-            val jsonData = data.groups["data"]?.value ?: error("Data not found")
+            val jsonData = data.groups["data"]?.value?.replace("\\'", "'") ?: error("Data not found")
             val translations = i18n.groups["translations"]?.value ?: error("Translations not found")
 
             return RewardState(
                 token,
-                DataCodec.CODEC.parse(JsonOps.INSTANCE, GsonHelper.parse(jsonData)).orThrow,
+                DataCodec.CODEC.parse(JsonOps.INSTANCE, GSON.fromJson(jsonData, JsonElement::class.java)).orThrow,
                 Language(translations)
             )
         }
